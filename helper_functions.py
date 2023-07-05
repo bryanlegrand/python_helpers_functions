779b2902e2aba6ead8a404908b9e37936df650d6
Bryan LE GRAND <97938874+rogueverve@users.noreply.github.com>
2023-07-05 16:28:53 +0400

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -12,6 +12,82 @@ from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 import os
 
 
+
+##############################################
+######## TIME SERIES HELPER FUNCTIONS ########
+##############################################
+
+def plot_time_series(timesteps, values, format=".", start=0, end=None, label=None):
+  """
+  Plots timesteps (a series of points in time) against values (a series of values across time)
+
+  Parameters
+  ----------
+  timesteps : array of timestep values
+  values : array of values across time
+  format : style of plot, default "."
+  start : where to start the plot (setting will index on our timesteps array)
+  end : where to end the plot (setting will index on our timesteps array)
+  label : label to show on plot about values, default None
+  """
+
+  # plot the series
+  plt.plot(timesteps[start:end], values[start:end], format, label=label)
+  plt.xlabel("Time")
+  plt.ylabel("Passengers")
+  if label:
+    plt.legend(fontsize=14)
+  plt.grid(True)
+
+
+# create function to label windowed data
+def get_labelled_windows(x, horizon=1):
+  """
+  Creates labels for windowed dataset.
+
+  E.g. if horizon=1 (default)
+  Input: [1, 2, 3, 4, 5, 6] -> Output: ([1, 2, 3, 4, 5], [6])
+  """
+  return x[:, :-horizon], x[:, -horizon:]
+
+
+# create function to view numpy arrays as windows
+def make_windows(x, window_size=7, horizon=1):
+  """
+  Turns a 1D array into a 2D array of sequential windows of window_size.
+  """
+  # 1. Create a window of specific window_size (add the horizon on the end for later labelling)
+  window_step = np.expand_dims(np.arange(window_size+horizon), axis=0)
+  # print(f"Window step:\n {window_step}")
+
+  # 2. Create a 2D array of multiple window steps (minus 1 to account for 0 indexing)
+  window_indexes = window_step + np.expand_dims(np.arange(len(x)-(window_size+horizon-1)), axis=0).T # create 2D array of windows of size window_size
+  # print(f"Window indexes:\n {window_indexes[:3], window_indexes[-3:], window_indexes.shape}")
+
+  # 3. Index on the target array (time series) with 2D array of multiple window steps
+  windowed_array = x[window_indexes]
+
+  # 4. Get the labelled windows
+  windows, labels = get_labelled_windows(windowed_array, horizon=horizon)
+
+  return windows, labels
+
+
+# make the train/test splits
+def make_train_test_splits(windows, labels, test_split=0.2):
+  """
+  Splits matching pairs of windows and labels into train and test splits.
+  """
+  split_size = int(len(windows) * (1-test_split)) # this will default to 80% train / 20% test
+  train_windows = windows[:split_size]
+  train_labels = labels[:split_size]
+  test_windows = windows[split_size:]
+  test_labels = labels[split_size:]
+  return train_windows, test_windows, train_labels, test_labels
+
+
+
+
 ##############################################
 ########### IMAGE HELPER FUNCTIONS ###########
 ##############################################
